b99b0eb59c3c03b18f4a8a97b75f2a387bdddb80
直挂云帆济沧海 <1913035339@qq.com>
2025-11-22 22:24:54 +0800

    Site updated: 2025-11-22 22:24:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/11/22/hello-world/"];function toRandomPost(){
+var posts=["2025/11/22/hello-world/","2025/11/22/第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
